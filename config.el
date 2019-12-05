4f7f01cea066799a3b249867eff2aee754064b04
Julian Lore <julian.lore@mail.mcgill.ca>
2019-12-05 04:17:09 -0500

    Add some functionality from original .emacs.d and replace C-x C-f

--- config.el
+++ config.el
@@ -5,4 +5,39 @@
       ;; Revert omni-completion C-x C-s
       (:when (featurep! :completion company)
         (:prefix "C-x"
+          :i "C-f"    #'find-file
           :i "C-s"    #'save-buffer)))
+
+(after! evil
+  (setq evil-want-fine-undo t))
+
+(after! undo-tree
+  (global-undo-tree-mode)
+  (setq undo-tree-auto-save-history t))
+
+(after! yasnippet
+  (setq yas-triggers-in-field t))
+
+(after! flyspell
+  (add-hook! prog-mode flyspell-prog-mode)
+  (add-hook! text-mode flyspell-mode))
+
+(use-package! flyspell-popup
+  :hook (flyspell-mode . flyspell-popup-auto-correct-mode))
+
+(global-set-key [3 67108911] (quote comment-line))
+  (global-set-key (kbd "C-c C-SPC") (quote comment-line))
+  (setq-default c-basic-offset 4 c-default-style "linux")
+  ;; Disable tab indentation, use spaces
+  (setq-default indent-tabs-mode nil)
+  ;; Tab size to be 4 spaces
+  (setq tab-stop-list (number-sequence 4 200 4))
+  (setq tab-width 4)
+
+  (when (fboundp 'windmove-default-keybindings)
+    (windmove-default-keybindings 'meta))
+
+  ;; Add auto-fill to text-mode by default
+  (add-hook 'text-mode-hook 'auto-fill-mode)
+;; Set yes-or-no to always be y-or-n
+(fset 'yes-or-no-p 'y-or-n-p)
