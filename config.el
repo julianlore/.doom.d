58041d67f4195e1534c18ca9503ef0114ca37ad8
Julian Lore <julian.lore@mail.mcgill.ca>
2019-12-05 03:16:02 -0500

    Overwrite C-x C-s back to saving instead of completion

--- config.el
+++ config.el
@@ -1,3 +1,8 @@
 ;;; .doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
+(map! :v  "@"     #'+evil:apply-macro
+      ;; Revert omni-completion C-x C-s
+      (:when (featurep! :completion company)
+        (:prefix "C-x"
+          :i "C-s"    #'save-buffer)))
